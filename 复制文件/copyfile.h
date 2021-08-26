e51c4cdcdb2a846a09a68abc3c62c11641bbce2c
GitHub-Xiong <52092280+GitHub-Xiong@users.noreply.github.com>
2021-08-26 16:44:26 +0800

    Update copyfile.h

--- 复制文件/copyfile.h
+++ 复制文件/copyfile.h
@@ -1,7 +1,10 @@
 #pragma once
+#ifndef _COPYFILE_H
+#define _COPYFILE_H
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 /*返回值  0 成功拷贝 1 文件名错误 2打开源文件失败
 3 打开目标文件失败 4 读文件错误 5 写文件错误*/
 int copyfile(char *SRCfile, char *targrtfile);
+#endif
